7886d09d2e0dc6beeb26089de61f1e3cd0a5a943
AnthonyOuzhene <anthony@DESKTOP-NLGLI39.localdomain>
2022-09-26 09:13:15 +0200

    make popup.js a service of vue

--- front/src/assets/js/popup.js
+++ front/src/assets/js/popup.js
@@ -1,37 +1,36 @@
-const popup = {
+// const popup = {
 
-    init: function() {
+//     init: function() {
 
-        const btnPopup = document.querySelectorAll('#btnPopup');
-        for (const currentPopup of btnPopup) {
-            console.log(currentPopup.classList[1])
-            currentPopup.addEventListener('click', () => {
-                popup.openModal(currentPopup.classList[1])
-            });
-        }  
+//         const btnPopup = document.querySelectorAll('#btnPopup');
+//         for (const currentPopup of btnPopup) {
+//             // console.log(currentPopup.classList[1])
+//             currentPopup.addEventListener('click', () => {
+//                 popup.openModal(currentPopup.classList[1])
+//             });
+//         }  
 
-        const btnClose = document.querySelectorAll('#btnClose');
-        for (const currentBtnClose of btnClose) {
-            currentBtnClose.addEventListener('click', popup.closePopup)}
-    },
+//         const btnClose = document.querySelectorAll('#btnClose');
+//         for (const currentBtnClose of btnClose) {
+//             currentBtnClose.addEventListener('click', popup.closePopup)}
+//     },
 
-    openModal: function(btnClassName) {
-        console.log(btnClassName)
-        const overlay = document.querySelector('.popup'+ btnClassName);
-        console.log(overlay)
-       // for (const currentOverlay of overlay) {
-        overlay.style.display = 'block';
-        //}
-        },
+//     openModal: function(btnClassName) {
+//         // console.log(btnClassName)
+//         const overlay = document.querySelector('.popup'+ btnClassName);
+        
+//        // for (const currentOverlay of overlay) {
+//         overlay.style.display = 'block';
+//         },
 
-    closePopup: function() {
-        console.log('test')
-        const AllOverlay = document.querySelectorAll('#overlay');
-        // on passe le display à none pour fermer la fenètre au click sur croix
-        for (let currentOverlay of AllOverlay) {
-            currentOverlay.style.display = 'none';
-        }
-        }
-}
+//     closePopup: function() {
+        
+//         const AllOverlay = document.querySelectorAll('#overlay');
+//         // on passe le display à none pour fermer la fenètre au click sur croix
+//         for (let currentOverlay of AllOverlay) {
+//             currentOverlay.style.display = 'none';
+//         }
+//         }
+// }
 
-document.addEventListener('DOMContentLoaded', popup.init);
+// document.addEventListener('DOMContentLoaded', popup.init);
